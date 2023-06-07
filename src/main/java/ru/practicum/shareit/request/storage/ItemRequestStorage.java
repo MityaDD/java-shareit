be0418e3483366e021ebaadec0defb1573a88e98
Mitya <ndvorkun@yandex.ru>
2023-06-07 13:47:25 +0300

    add request storage

---
+++ src/main/java/ru/practicum/shareit/request/storage/ItemRequestStorage.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.request.storage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import ru.practicum.shareit.request.model.ItemRequest;
+
+import java.util.List;
+
+
+public interface ItemRequestStorage extends JpaRepository<ItemRequest, Long> {
+
+    List<ItemRequest> findAllByRequesterOrderByCreatedDesc(Long userId);
+                    //findAllByRequesterIdOrderByCreatedDesc
+
+    List<ItemRequest> findAllByRequesterIsNotOrderByCreatedDesc(Long requester, Pageable page);
+    //              findAllByRequesterIdIsNotOrderByCreatedDesc
+}
