ca771f297bad60ebce66314d904a71ca70c602cd
Mitya <ndvorkun@yandex.ru>
2023-06-20 00:15:09 +0300

    initial commit

--- src/main/java/ru/practicum/shareit/user/storage/UserStorage.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.user.storage;
-
-import ru.practicum.shareit.user.model.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserStorage extends JpaRepository<User, Long>  {
-}
